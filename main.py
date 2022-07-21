df15d03a4c2a82735a0d82dde3d33af822679964
padsalatushal <padsalatushal@gmail.com>
2022-07-21 23:09:47 +0530

    Added function for setup chrome reg entry

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
 import winreg
 
-# with winreg.ConnectRegistry(None, winreg.HKEY_CLASSES_ROOT) as hkey:
-#     key =  winreg.CreateKey(hkey,'Directory\\Background\\shell\\new_test') 
-#     winreg.CloseKey(key)
+'''
+# basic way to add key in registry
+with winreg.ConnectRegistry(None, winreg.HKEY_CLASSES_ROOT) as hkey: #get the neceessary hkey
+    key =  winreg.CreateKey(hkey,'Directory\\Background\\shell\\new_test') 
+    winreg.CloseKey(key)
 
+# Add key in registry Using function
 def create_key():
-    key = winreg.CreateKey(winreg.HKEY_CLASSES_ROOT,'Directory\\Background\\shell\\new_test\\command') 
+    key = winreg.CreateKey(winreg.HKEY_CLASSES_ROOT,'Directory\\Background\\shell\\Chrome\\command') 
     winreg.SetValue(key, '', winreg.REG_SZ, 'C:\Program Files\Google\Chrome\Application\chrome.exe')
     winreg.CloseKey(key)
 create_key()
+
+# for adding icon 
+key = winreg.OpenKey(winreg.HKEY_CLASSES_ROOT,'Directory\\Background\\shell\\Chrome\\', 0, winreg.KEY_ALL_ACCESS)
+winreg.SetValueEx(key, 'icon', 0, winreg.REG_SZ, 'C:\Program Files\Google\Chrome\Application\chrome.exe')
+winreg.CloseKey(key)
+
+# Delete key
+key = 'Directory\\Background\\shell\\Chrome\\command'
+winreg.DeleteKey(winreg.HKEY_CLASSES_ROOT,key)
+
+'''
+
+def Chrome():
+    key = winreg.CreateKey(winreg.HKEY_CLASSES_ROOT,'Directory\\Background\\shell\\Chrome') 
+    
+    winreg.SetValue(key, 'command', winreg.REG_SZ, 'C:\Program Files\Google\Chrome\Application\chrome.exe')
+
+    winreg.SetValueEx(key, 'icon', 0, winreg.REG_SZ, 'C:\Program Files\Google\Chrome\Application\chrome.exe')
+    
+    winreg.CloseKey(key)
+Chrome()
