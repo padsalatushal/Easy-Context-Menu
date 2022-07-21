35c806a176d931b8c0de324b127b2f6390eee232
padsalatushal <padsalatushal@gmail.com>
2022-07-21 20:40:57 +0530

    added function for adding key

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import winreg
 
-with winreg.ConnectRegistry(None, winreg.HKEY_CLASSES_ROOT) as hkey:
-    key =  winreg.CreateKey(hkey,'Directory\\Background\\shell\\new_test') 
+# with winreg.ConnectRegistry(None, winreg.HKEY_CLASSES_ROOT) as hkey:
+#     key =  winreg.CreateKey(hkey,'Directory\\Background\\shell\\new_test') 
+#     winreg.CloseKey(key)
+
+def create_key():
+    key = winreg.CreateKey(winreg.HKEY_CLASSES_ROOT,'Directory\\Background\\shell\\new_test\\command') 
+    winreg.SetValue(key, '', winreg.REG_SZ, 'C:\Program Files\Google\Chrome\Application\chrome.exe')
     winreg.CloseKey(key)
+create_key()
