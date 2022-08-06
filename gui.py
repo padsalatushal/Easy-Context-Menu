4cb7dbbc839485c29b56a368ccdbf40149f363f6
padsalatushal <padsalatushal@gmail.com>
2022-08-06 13:15:11 +0530

    added functions & button for removing the keys

--- gui.py
+++ gui.py
@@ -30,6 +30,7 @@ def apply():
         print('Done')
     elif var1.get()==0:
         print('noting happen')
+        
     
 
 
@@ -37,5 +38,56 @@ def apply():
 Checkbutton(root, text='chrome', font='18', variable=var1).grid(row=1, column=0)
 Button(root, text='show', command=var_state).grid(row=2, sticky=W, column=2)
 Button(root, text='Apply', command=apply).grid(row=2, sticky=W, column=1)
+
+
+
+
+def delete_sub_key(key0, current_key):
+
+    open_key = winreg.OpenKey(key0, current_key, 0,
+                              winreg.KEY_ALL_ACCESS)
+    info_key = winreg.QueryInfoKey(open_key)
+    for x in range(0, info_key[0]):
+        # NOTE:: This code is to delete the key and all sub_keys.
+        # If you just want to walk through them, then
+        # you should pass x to EnumKey. sub_key = winreg.EnumKey(open_key, x)
+        # Deleting the sub_key will change the sub_key count used by EnumKey.
+        # We must always pass 0 to EnumKey so we
+        # always get back the new first sub_key.
+        sub_key = winreg.EnumKey(open_key, 0)
+        try:
+            winreg.DeleteKey(open_key, sub_key)
+            print("Removed %s\\%s " % (current_key, sub_key))
+        except OSError:
+            delete_sub_key(key0, "\\".join([current_key, sub_key]))
+            # No extra delete here since each call
+            # to delete_sub_key will try to delete itself when its empty.
+
+    winreg.DeleteKey(open_key, "")
+    open_key.Close()
+    print("Removed %s" % current_key)
+    return
+
+
+
+# List of keys to delete
+chrome_path = 'Directory\Background\shell\Chrome'
+keys = []
+
+def delete_key():
+    for key in keys:
+        try:
+            delete_sub_key(winreg.HKEY_CLASSES_ROOT, key)
+        except OSError as e:
+            print(e)
+
+def remove():
+    if var1.get()==1:
+        keys.append(chrome_path)
+        print(keys)
+        delete_key()
+        print("Delete Done")
+
+Button(root, text='remove', command=remove).grid(row=3, sticky=W, column=2)
 
 root.mainloop()
