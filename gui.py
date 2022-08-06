4a57d296fb5b97adb0a8e9509860715fef969cbb
padsalatushal <padsalatushal@gmail.com>
2022-08-06 13:29:21 +0530

    added task manager checkbox & improve the method for adding and removing

--- gui.py
+++ gui.py
@@ -7,7 +7,6 @@ root.geometry('500x400')
 root.minsize(400, 300)
 root.title('Easy Context Menu')
 
-
 Label(root, text='Easy Context Menu', font='comicsansms 29 bold').grid(row=0, column=2)
 
 def Chrome():
@@ -19,27 +18,31 @@ def Chrome():
                       'C:\Program Files\Google\Chrome\Application\chrome.exe')
     winreg.CloseKey(key)
 
+def task_manager():
+    key = winreg.CreateKey(winreg.HKEY_CLASSES_ROOT, 'Directory\\Background\\shell\\task_manager')
+    winreg.SetValue(key, 'command', winreg.REG_SZ, 'C:\Windows\system32\Taskmgr.exe')
+    winreg.SetValueEx(key, '', 0, winreg.REG_SZ, 'Task Manager')
+    winreg.SetValueEx(key, 'icon', 0, winreg.REG_SZ, 'C:\Windows\system32\Taskmgr.exe')
+    winreg.CloseKey(key)
+
+chrome_check_var = IntVar()
+task_manager_check_var = IntVar()
 
 def var_state():
-    print(var1.get())
-var1 = IntVar()
+    print("chrome : ",chrome_check_var.get())
+    print("Task Manager : ",task_manager_check_var.get())
 
 def apply():
-    if var1.get()==1:
+    if chrome_check_var.get()==1:
         Chrome()
-        print('Done')
-    elif var1.get()==0:
-        print('noting happen')
-        
-    
+    if task_manager_check_var.get()==1:
+        task_manager()
 
+Checkbutton(root, text='Chrome', font='18', variable=chrome_check_var).grid(row=1, column=0)
+Checkbutton(root, text='Task Manager', font='18', variable=task_manager_check_var).grid(row=2, column=0)
 
-
-Checkbutton(root, text='chrome', font='18', variable=var1).grid(row=1, column=0)
 Button(root, text='show', command=var_state).grid(row=2, sticky=W, column=2)
 Button(root, text='Apply', command=apply).grid(row=2, sticky=W, column=1)
-
-
 
 
 def delete_sub_key(key0, current_key):
@@ -57,7 +60,7 @@ def delete_sub_key(key0, current_key):
         sub_key = winreg.EnumKey(open_key, 0)
         try:
             winreg.DeleteKey(open_key, sub_key)
-            print("Removed %s\\%s " % (current_key, sub_key))
+            # print("Removed %s\\%s " % (current_key, sub_key))
         except OSError:
             delete_sub_key(key0, "\\".join([current_key, sub_key]))
             # No extra delete here since each call
@@ -65,13 +68,13 @@ def delete_sub_key(key0, current_key):
 
     winreg.DeleteKey(open_key, "")
     open_key.Close()
-    print("Removed %s" % current_key)
+    # print("Removed %s" % current_key)
     return
-
 
 
 # List of keys to delete
 chrome_path = 'Directory\Background\shell\Chrome'
+task_manager_path = 'Directory\\Background\\shell\\task_manager'
 keys = []
 
 def delete_key():
@@ -82,11 +85,12 @@ def delete_key():
             print(e)
 
 def remove():
-    if var1.get()==1:
+    if chrome_check_var.get()==1:
         keys.append(chrome_path)
-        print(keys)
-        delete_key()
-        print("Delete Done")
+    if task_manager_check_var.get()==1:
+        keys.append(task_manager_path)    
+    delete_key()
+
 
 Button(root, text='remove', command=remove).grid(row=3, sticky=W, column=2)
 
