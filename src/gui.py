834c198b07b9f21b72bcfbdd94676ef948522708
padsalatushal <padsalatushal@gmail.com>
2022-08-09 16:36:08 +0530

    added shutdown,show hidden files toggle & delete tmp file checkbox

--- src/gui.py
+++ src/gui.py
@@ -4,7 +4,7 @@ import winreg
 root = Tk()
 
 
-root.geometry('550x400')
+root.geometry('750x400')
 root.minsize(400, 300)
 root.title('Easy Context Menu')
 
@@ -78,11 +78,65 @@ def select_all():
     winreg.CloseKey(key)
     winreg.CloseKey(key1)
 
+
+
+# https://helpdeskgeek.com/how-to/add-shutdown-and-restart-to-the-right-click-context-menu-in-windows-8/
+
+def shutdown():
+    key = winreg.CreateKey(winreg.HKEY_CLASSES_ROOT,
+                           'Directory\\Background\\shell\\Shutdown')
+    winreg.SetValue(key, 'command', winreg.REG_SZ, 'shutdown -s -t 00 -f')
+    winreg.SetValueEx(key, 'Icon', 0, winreg.REG_SZ, 'shell32.dll,-221')
+    winreg.SetValueEx(key, 'Position', 0, winreg.REG_SZ, 'Bottom')
+    winreg.CloseKey(key)
+
+
+# https://superuser.com/questions/1151844/how-to-toggle-show-hide-hidden-files-in-windows-through-command-line
+
+def show_hidden_files_toggle():
+    key1 = winreg.CreateKey(winreg.HKEY_CLASSES_ROOT,
+                            'Folder\\shell\\Windows.ShowHiddenFiles')
+    key2 = winreg.CreateKey(winreg.HKEY_CLASSES_ROOT,
+                            'Directory\\Background\\shell\\Windows.ShowHiddenFiles')
+
+    winreg.SetValueEx(key1, 'CommandStateSync', 0, winreg.REG_SZ, '')
+    winreg.SetValueEx(key1, 'Description', 0,
+                      winreg.REG_SZ, '@shell32.dll,-37573')
+    winreg.SetValueEx(key1, 'ExplorerCommandHandler', 0,
+                      winreg.REG_SZ, '{f7300245-1f4b-41ba-8948-6fd392064494}')
+    winreg.SetValueEx(key1, 'MUIVerb', 0, winreg.REG_SZ, '@shell32.dll,-37572')
+
+    winreg.SetValueEx(key2, 'CommandStateSync', 0, winreg.REG_SZ, '')
+    winreg.SetValueEx(key2, 'Description', 0,
+                      winreg.REG_SZ, '@shell32.dll,-37573')
+    winreg.SetValueEx(key2, 'ExplorerCommandHandler', 0,
+                      winreg.REG_SZ, '{f7300245-1f4b-41ba-8948-6fd392064494}')
+    winreg.SetValueEx(key2, 'MUIVerb', 0, winreg.REG_SZ, '@shell32.dll,-37572')
+
+    winreg.CloseKey(key1)
+    winreg.CloseKey(key2)
+
+
+def delete_tmp_file():
+    key = winreg.CreateKey(winreg.HKEY_CLASSES_ROOT,
+                           'Directory\\Background\\shell\\delete_tmp_file')
+    winreg.SetValueEx(key, '', 0, winreg.REG_SZ, 'Clean Temporary Files')
+    winreg.SetValue(key, 'command', winreg.REG_SZ,
+                    'cmd.exe /C del /q/f/s %temp%\*')
+    winreg.CloseKey(key)
+
+
+
+
 chrome_check_var = IntVar()
 task_manager_check_var = IntVar()
 copy_as_path_check_var = IntVar()
 delete_permanetly_check_var = IntVar()
 select_all_check_var = IntVar()
+shutdown_check_var = IntVar()
+show_hidden_files_toggle_check_var = IntVar()
+delete_tmp_file_check_var = IntVar()
+
 
 def var_state():
     print("chrome : ",chrome_check_var.get())
@@ -90,6 +144,9 @@ def var_state():
     print("Copy As Path: ",copy_as_path_check_var.get())
     print("Delete permanetly :", delete_permanetly_check_var.get())
     print("Select All :", select_all_check_var.get())
+    print("Shutdown :",shutdown_check_var.get())
+    print("Show Hidden items toggle :", show_hidden_files_toggle_check_var.get())
+    print("Delete tmp file:", delete_tmp_file_check_var.get())
 
 def apply():
     if chrome_check_var.get()==1:
@@ -102,13 +159,21 @@ def apply():
         delete_permanetly()
     if select_all_check_var.get()==1:
         select_all()
+    if shutdown_check_var.get()==1:
+        shutdown()
+    if show_hidden_files_toggle_check_var.get()==1:
+        show_hidden_files_toggle()
+    if delete_tmp_file_check_var.get()==1:
+        delete_tmp_file()
 
 Checkbutton(root, text='Chrome', font='18', variable=chrome_check_var).grid(row=1, column=0)
 Checkbutton(root, text='Task Manager', font='18', variable=task_manager_check_var).grid(row=2, column=0)
 Checkbutton(root, text='Copy As Path', font='18', variable=copy_as_path_check_var).grid(row=3, column=0)
 Checkbutton(root, text='Delete Permanetly', font='18', variable=delete_permanetly_check_var).grid(row=1, column=1)
 Checkbutton(root, text='Select All', font='18', variable=select_all_check_var).grid(row=2, column=1)
-
+Checkbutton(root, text='Shutdown', font='18', variable=shutdown_check_var).grid(row=1, column=3)
+Checkbutton(root, text='Show hidden item toggle', font='18', variable=show_hidden_files_toggle_check_var).grid(row=2, column=3)
+Checkbutton(root, text='Delete Temporary files', font='18', variable=delete_tmp_file_check_var).grid(row=3, column=3)
 
 Button(root, text='show', command=var_state).grid(row=6, sticky=W, column=0)
 Button(root, text='Apply', command=apply).grid(row=6, sticky=W, column=1)
@@ -147,6 +212,10 @@ task_manager_path = 'Directory\\Background\\shell\\task_manager'
 copy_as_path_path = 'Allfilesystemobjects\\shell\\windows.copyaspath'
 delete_permanetly_path = 'AllFileSystemObjects\\shell\\delete_permanetly'
 select_all_path = 'Directory\\Background\\shell\\Windows.selectall'
+shutdown_path = 'Directory\\Background\\shell\\Shutdown'
+show_hidden_files_toggle_path = 'Folder\\shell\\Windows.ShowHiddenFiles'
+show_hidden_files_toggle_path1 = 'Directory\\Background\\shell\\Windows.ShowHiddenFiles'
+delete_tmp_file_path = 'Directory\\Background\\shell\\delete_tmp_file'
 
 keys = []
 
@@ -168,7 +237,16 @@ def remove():
         keys.append(delete_permanetly_path)
     if select_all_check_var.get()==1:
         keys.append(select_all_path)
-    
+    if shutdown_check_var.get()==1:
+        keys.append(shutdown_path)
+    if show_hidden_files_toggle_check_var.get()==1:
+        keys.append(show_hidden_files_toggle_path)
+        keys.append(show_hidden_files_toggle_path1)
+    if delete_tmp_file_check_var.get()==1:
+        keys.append(delete_tmp_file_path)
+
+
+
     delete_key()
 
 
